0c3684ac4428a6a3b62e96b1a2590d9caef18488
Aidan Delaney <aidan@ontologyengineering.org>
2016-04-19 13:06:21 +0100

    Updated svg 2 png generation to latest pandoc API

--- pandoc-svg.py
+++ pandoc-svg.py
@@ -10,33 +10,38 @@ import mimetypes
 import subprocess
 import os
 import sys
-from pandocfilters import toJSONFilter, Str, Para, Image
+from pandocfilters import toJSONFilter, Image
 
-#TODO add emf export if fmt=="docx" ?
+# TODO add emf export if fmt=="docx" ?
 fmt_to_option = {
-    "latex": ("--export-pdf","pdf"),
-    "beamer": ("--export-pdf","pdf"),
-    #because of IE
+    "latex": ("--export-pdf", "pdf"),
+    "beamer": ("--export-pdf", "pdf"),
+    # because of IE
     "html": ("--export-png", "png")
 }
 
+
 def svg_to_any(key, value, fmt, meta):
     if key == 'Image':
-       alt, [src, title] = value
-       mimet,_ = mimetypes.guess_type(src)
-       option = fmt_to_option.get(fmt)
-       if mimet == 'image/svg+xml' and option:
-           base_name,_ = os.path.splitext(src)
-           eps_name = base_name + "." + option[1]
-           try:
-               mtime = os.path.getmtime(eps_name)
-           except OSError:
-               mtime = -1
-           if mtime < os.path.getmtime(src):
-               cmd_line = ['inkscape', option[0], eps_name, src]
-               sys.stderr.write("Running %s\n" % " ".join(cmd_line))
-               subprocess.call(cmd_line, stdout=sys.stderr.fileno())
-           return Image(alt, [eps_name, title])
+        attrs, alt, [src, title] = value
+        mimet, _ = mimetypes.guess_type(src)
+        option = fmt_to_option.get(fmt, ("--export-pdf", "pdf"))
+        if mimet == 'image/svg+xml' and option:
+            base_name, _ = os.path.splitext(src)
+            eps_name = base_name + "." + option[1]
+            eps_name = eps_name.replace("%20", "")            
+            src = src.replace("%20", " ")
+            try:
+                mtime = os.path.getmtime(eps_name)
+            except OSError:
+                mtime = -1
+            if mtime < os.path.getmtime(src):
+                cmd_line = ['inkscape', option[0], eps_name, src]
+                sys.stderr.write("Running %s\n" % " ".join(cmd_line))
+                subprocess.call(cmd_line, stdout=sys.stderr.fileno())
+            return Image(attrs, alt, [eps_name.replace("%20", " "), title])
 
 if __name__ == "__main__":
-  toJSONFilter(svg_to_any)
+    toJSONFilter(svg_to_any)
+
+
